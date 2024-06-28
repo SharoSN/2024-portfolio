cb99c5816d6c06f68e59c12ce25b74d4f585eac7
Sharo <redshanks@Sharos-MacBook-Pro.local>
2024-06-28 19:04:35 -0400

    update

--- app/actions.js
+++ app/actions.js
@@ -12,7 +12,7 @@ export async function generateAnswer(prompt) {
   Age: 29 years old
   height: 1.87m
   languages: German, English, Farsi
-  Favourite Boxer: Mohammed Ali
+  Favourite Boxer: Muhammad Ali
   Eye Color: Dark Brown
   
   Background: From Germany but live in Montreal Canada, I am born in Russia my My mother is from russia and my father is from Afghanistan.
@@ -23,7 +23,7 @@ export async function generateAnswer(prompt) {
   Description: Sahil Noorzai is a web developer specializing in integrating large language models (LLMs) into web applications using LangChain.js.
   Hobbies: Sahil likes boxing and loves playing the guitar.`;
 
-  prompt = `Given the following personal information, generate an answer to the question: "${prompt}". The answer should be in a JSON array with an object containing 'description' fields.
+  prompt = `Given the following personal information, generate an answer to the question: "${prompt}". The answer should be in a JSON array with an object containing 'description' fields and answer specificly asked question.
   Personal Information: ${personalInfo}`;
 
   const response = await chatModel.invoke(prompt);
